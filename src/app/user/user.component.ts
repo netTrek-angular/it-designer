5b95233c21362acd4ef28ce21d2dffe1dff60973
Saban Ünlü [netTrek] <us@netTrek.de>
2022-03-29 16:34:59 +0200

    user style

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'itd-user',
   templateUrl: './user.component.html',
   // template: '<p><strong>hello</strong> world</p>',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class UserComponent implements OnInit {
+
+  name = "Saban";
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  chgName() {
+    this.name = 'Peter';
+  }
 }
