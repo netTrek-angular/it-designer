7f3b8c961c724b142a27e2a722e8947cd6916139
Saban Ünlü [netTrek] <us@netTrek.de>
2022-03-29 17:01:29 +0200

    bindings sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
+import { BindingSamplesModule } from './binding-samples/binding-samples.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     AppRoutingModule,
     UserModule,
+    BindingSamplesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/binding-samples/binding-samples.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BindingSamplesComponent } from './binding-samples.component';
+
+describe('BindingSamplesComponent', () => {
+  let component: BindingSamplesComponent;
+  let fixture: ComponentFixture<BindingSamplesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BindingSamplesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BindingSamplesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
