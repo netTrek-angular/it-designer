35023b7800f4b6fb2aeb0681a9e1157087f5d3db
Saban Ünlü [netTrek] <us@netTrek.de>
2022-03-30 10:55:59 +0200

    countdown.component.ts sample

--- src/app/binding-samples/binding-samples.component.ts
+++ src/app/binding-samples/binding-samples.component.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BindingSamplesComponent implements OnInit {
 
+  name: string = 'Saban';
+  num: number = 300;
+  crrImage: number = 200300;
+  html: string = `<p>
+Hello <strong>World</strong> <script>alert('hacked')</script>
+</p>`;
+  fontColor: number|string = 'blue';
   constructor() { }
 
   ngOnInit(): void {
+    console.log ( 'init interval')
+    // setInterval( () => {
+    //   this.increment( 10 );
+    // }, 500 )
+
+  }
+  /*
+  // baut eine Comp
+  // in der komp sind zwei vollflächige Vierecke
+  // rotes Viereck liegt im grünen
+  // innerhalb von 5 sekunden soll das rote viereck bis auf eine Breite von 0%
+  // animiert verkleinert werden
+
+  ----------------
+  | grün          |
+  ----------------
+
+----------------
+| grün   | rot |
+----------------
+
+----------------
+| rot          |
+----------------
+*/
+  getName(prefix: string = '', num?: number ) {
+    // console.log( 'getName' );
+    // return prefix + this.name
+    return `${prefix} ${this.name} ${num??0}`
   }
 
+  increment( val: number = 1) {
+    // console.log( 'increment' );
+    this.num = this.num + val ;
+  }
+
+  chgImg() {
+    if ( this.crrImage > 200300 ) {
+      this.crrImage -= 100000;
+    } else {
+      this.crrImage += 100000;
+    }
+  }
+
+  chgColor() {
+    this.fontColor = this.fontColor === 'red' ? 'blue' : 'red';
+  }
 }
--- src/app/binding-samples/binding-samples.module.ts
+++ src/app/binding-samples/binding-samples.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BindingSamplesComponent } from './binding-samples.component';
+import {CountdownComponent} from "./countdown/countdown.component";
 
 
 
 @NgModule({
   declarations: [
-    BindingSamplesComponent
+    BindingSamplesComponent, CountdownComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    BindingSamplesComponent
+    BindingSamplesComponent, CountdownComponent
   ]
 })
 export class BindingSamplesModule { }
